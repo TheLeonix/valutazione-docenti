2357003abca55180b7fda3851691381fbbd6e736
TheLeonix <leon.canino06@gmail.com>
2024-01-01 05:24:59 +0100

    Added Multipage

--- src/App.js
+++ src/App.js
@@ -1,64 +1,21 @@
-import React, { useState, useEffect } from 'react';
-
-function ValutazioneDocenti() {
-  console.clear()
-  const [docenti, setDocenti] = useState([]);
-  // Robe Temporanee
-  const utente = {
-    classe: 'Classe4L',
-  };
-  // Fine RobeTemporanee
-  useEffect(() => {
-    fetch('https://raw.githubusercontent.com/1Lg20/ValutazioneDocenti/main/ProfJSON.json')
-      .then(response => response.json())
-      .then(data => {
-        const docentiFiltrati = data.filter(docente => docente.materie[utente.classe]);
-        setDocenti(docentiFiltrati);
-      })
-      .catch(error => console.error('Errore durante il recupero dei dati:', error));
-  }, [utente.classe]);
-  const setTrue = (docenteId) => {
-    setDocenti((docentiAttuali) => {
-      return docentiAttuali.map((docente) => {
-        if (docente.nome === docenteId) {
-          return { ...docente, valutato: true };
-        }
-        return docente;
-      });
-    });
-  };
-  const Test=(valutato,nome)=>{
-    console.log("entrato")
-    if(valutato===false)
-    {
-      return (
-        <input type='button' value={"Valuta"} onClick={()=>setTrue(nome)}/>
-      )
-    }
-    else
-    {
-      return(
-      
-          <div>Già Votato</div>
-      )
-    }
-  }
-  return (
-    <div className="container mt-5">
-      <h1 className="mb-4">Valutazione Docenti</h1>
-
-      <ul className="list-group">
-        {docenti.map(docente => (
-          <li className="list-group-item">
-            <h5>{docente.nome}</h5>
-            <p>{docente.materie[utente.classe]}</p>
-            {Test(docente.valutato,docente.nome)}
-            {console.log(docente.valutato)}
-            
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./Pag/Home";
+import Login from "./Pag/Login";
+ 
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
 }
-export default ValutazioneDocenti;
+ 
+export default App;
---
+++ src/Pag/Login.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function Login() {
+  console.clear()
+  return (
+    <div className="container mt-5">
+        CIao
+    </div>
+  );
+}
+export default Login;
